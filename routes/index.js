b071504c2fd62763af102012f3d6b5f75fcf019e
Syaeful Bahri <syaefulbahri@protonmail.com>
2023-04-05 19:36:14 +0700

    Nambain routing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index");
+});
+
+router.get("/demographics", function (req, res) {
+  res.render("demographics");
+});
+
+router.get("/ict-users", function (req, res) {
+  res.render("ict-users");
+});
+
+router.get("/ict-professionals", function (req, res) {
+  res.render("ict-professionals");
+});
+
+router.get("/digital-infra", function (req, res) {
+  res.render("digital-infra");
+});
+
+router.get("/digital-culture", function (req, res) {
+  res.render("digital-culture");
 });
 
 module.exports = router;
